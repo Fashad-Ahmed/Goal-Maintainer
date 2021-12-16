9247ebaf32d6036c6968df9f5400ac3831506b70
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2021-12-16 11:53:25 +0500

    Module 1

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [outputText, setOutputText] = useState(
+    "Open up App.js to start working on your app!"
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {StatusBar}
+      <Text>{outputText}</Text>
+      <Button
+        title="changeText"
+        onPress={() => setOutputText("Button Clicked.")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
